1b32555094bcec0892a105d51aba4b7e9537515a
陈靖宇 <1993456181@qq.com>
2021-12-08 21:14:35 +0800

    禁止微信浏览器调整字体大小

--- src/general/pxtorem.js
+++ src/general/pxtorem.js
@@ -3,6 +3,51 @@ const setView = () => {
     //设置html标签的fontSize
     document.documentElement.style.fontSize = (100 * screen.width / WIDTH) + 'px';
 };
-screen.orientation.onchange = setView;
 window.onresize = setView;
 setView();
+
+// 禁止微信浏览器调整字体大小
+
+(function () {
+
+    if (typeof WeixinJSBridge == "object" && typeof WeixinJSBridge.invoke == "function") {
+
+        handleFontSize();
+
+    } else {
+        if (document.addEventListener) {
+
+            document.addEventListener("WeixinJSBridgeReady", handleFontSize, false);
+
+        } else if (document.attachEvent) {
+
+            document.attachEvent("WeixinJSBridgeReady", handleFontSize);
+
+            document.attachEvent("onWeixinJSBridgeReady", handleFontSize);
+
+        }
+
+    }
+
+    function handleFontSize() {
+
+        // 设置网页字体为默认大小
+        WeixinJSBridge.invoke('setFontSizeCallback', {
+
+            'fontSize': 0
+
+        });
+
+        // 重写设置网页字体大小的事件
+        WeixinJSBridge.on('menu:setfont', function () {
+
+            WeixinJSBridge.invoke('setFontSizeCallback', {
+
+                'fontSize': 0
+
+            });
+
+        });
+
+    }
+})();
