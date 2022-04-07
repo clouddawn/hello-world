61b24780e227f6df220b2df31ec6aeb5e3196f62
clouddawn <1993456181@qq.com>
2022-04-07 17:03:59 +0800

    删除时间戳转换函数，以后改用dayjs

--- src/general/commonFunction.js
+++ src/general/commonFunction.js
@@ -2,55 +2,6 @@
 import {Dialog, Toast} from 'vant';
 
 export default {
-  // 时间戳转换
-  // 2022-01-27 10:10:59 格式
-  timestampTransform(time) {
-    let date = new Date(time);
-    let Y = date.getFullYear() + "-";
-    let M =
-      (date.getMonth() + 1 < 10
-        ? "0" + (date.getMonth() + 1)
-        : date.getMonth() + 1) + "-";
-    let D = (date.getDate() < 10 ? "0" + date.getDate() : date.getDate()) + " ";
-    let h =
-      (date.getHours() < 10 ? "0" + date.getHours() : date.getHours()) + ":";
-    let m =
-      (date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()) +
-      ":";
-    let s =
-      date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
-    let completeTime = Y + M + D + h + m + s;
-    let inCompleteTime = Y + M + D;
-    return {
-      completeTime,
-      inCompleteTime,
-    };
-  },
-  // 时间戳转换
-  // 2022年01月27日 10:10:59 格式
-  timestampTransformZh(time) {
-    let date = new Date(time);
-    let Y = date.getFullYear() + "年";
-    let M =
-      (date.getMonth() + 1 < 10
-        ? "0" + (date.getMonth() + 1)
-        : date.getMonth() + 1) + "月";
-    let D =
-      (date.getDate() < 10 ? "0" + date.getDate() : date.getDate()) + "日 ";
-    let h =
-      (date.getHours() < 10 ? "0" + date.getHours() : date.getHours()) + ":";
-    let m =
-      (date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()) +
-      ":";
-    let s =
-      date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
-    let completeTime = Y + M + D + h + m + s;
-    let inCompleteTime = Y + M + D;
-    return {
-      completeTime,
-      inCompleteTime,
-    };
-  },
   // 获取高度实现滑轮监听
   setHeight(name) {
     // 接受一个CSS选择器字符串，用于匹配dom元素，在 mounted 里面调用这段代码
